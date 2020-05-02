8863028de0ff19271e12cdc09b0a4b93a8376218
Vitalii Drag <ruban.vitaliy@gmail.com>
2020-05-02 18:34:56 +0300

    Added throttling support Added CLI support for throttling

--- cliOptions.js
+++ cliOptions.js
@@ -1,6 +1,7 @@
 const options = [
     { name: 'url', alias: 'U', type: String, defaultOption: true },
     { name: 'runs', alias: 'R', type: Number, defaultValue: 3 },
+    { name: 'throttling', alias: 'T', type: (v) => v === 'default' ? 'default' : null }
 ]
 
 function validate(opt) {
@@ -11,7 +12,16 @@ function validate(opt) {
     return opt;
 }
 
+const defaultNetworkConditions = { // TODO: Make it a) optional or loadable from testsuite
+    CPUThrottlingRate: 4,
+    offline: false,
+    latency: 200, // ms
+    downloadThroughput: 780 * 1024 / 8, // 780 kb/s
+    uploadThroughput: 330 * 1024 / 8, // 330 kb/s
+}
+
 module.exports = {
     options,
+    defaultNetworkConditions,
     validate
 };
